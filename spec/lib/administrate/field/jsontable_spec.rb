147d4b69c80386499bd7371fff6fe8ca3bf065ea
Murat Toygar <toygar-murat@hotmail.com>
2020-07-21 14:05:36 +0300

    support index pages for Jsontable field

--- spec/lib/administrate/field/jsontable_spec.rb
+++ spec/lib/administrate/field/jsontable_spec.rb
@@ -3,12 +3,21 @@ require 'administrate/field/jsontable'
 describe Administrate::Field::Jsontable do
   describe '#to_partial_path' do
     it 'returns a partial based on the page being rendered' do
-      page = :show
-      field = Administrate::Field::Jsontable.new(:status, 'status', page)
-
-      path = field.to_partial_path
-
-      expect(path).to eq("/fields/jsontable/#{page}")
+      assert_partial_path(page: :show)
     end
+
+    it 'returns a partial for index page' do
+      assert_partial_path(page: :index)
+    end
+  end
+
+  private
+
+  def assert_partial_path(page:)
+    field = Administrate::Field::Jsontable.new(:status, 'status', page)
+
+    path = field.to_partial_path
+
+    expect(path).to eq("/fields/jsontable/#{page}")
   end
 end
